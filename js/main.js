c14471a318c7b09a1cfb2b31ae7cd63096395089
Danil <daniklucky38@gmail.com>
2020-05-06 11:33:28 +0400

    change style

--- js/main.js
+++ js/main.js
@@ -2,11 +2,9 @@ const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
 
-cartButton.addEventListener("click", function (event) {
-   modal.classList.add("is-open");
-});
-close.addEventListener("click", function (event) {
-   modal.classList.remove("is-open");
-});
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
 
-new WOW().init();
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
